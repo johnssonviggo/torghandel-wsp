0aaee108adb53288fc476b3cfbe06de7298f1b0d
johnssonviggo <viggo.johnsson1@gmail.com>
2025-03-26 20:13:39 +0100

    MVC struktur fixad

--- torghandel-backend/app.rb
+++ torghandel-backend/app.rb
@@ -1,10 +1,3 @@
-require 'json'
-require 'sinatra'
-require 'sinatra/cross_origin'
-require 'bcrypt'
-
-require_relative './models/listings'
-
 require_relative 'controller/application_controller'
 require_relative 'controller/users_controller'
 require_relative 'controller/listings_controller'
